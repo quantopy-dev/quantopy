2c2797868633b9db5155f4b6edd964de9afe7e06
Ismael Jimenez <ijimenezm@hotmail.es>
2021-05-23 19:15:14 +0200

    added simple returns

---
+++ quantopy/returns.py
@@ -0,0 +1,25 @@
+from pandas.core.generic import NDFrame
+
+
+def returns(prices: NDFrame, drop_first: bool = True) -> NDFrame:
+    """
+    Compute simple returns from a timeseries of prices.
+
+    Parameters
+    ----------
+    prices : pd.Series or pd.DataFrame
+        Prices of assets in wide-format, with assets as columns,
+        and indexed by datetimes.
+
+    Returns
+    -------
+        returns : Series or DataFrame
+            The same type as the calling object.
+
+    """
+    out = prices.pct_change()
+
+    if drop_first:
+        out = out.iloc[1:]
+
+    return out
---
+++ tests/test_returns.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import pandas._testing as tm
+from numpy import NaN
+
+from quantopy import returns
+
+
+class TestReturns:
+    def test_series_returns(self):
+        # Introduction to Portfolio Construction and Analysis with Python. EDHEC-Risk
+        prices_1 = pd.Series([8.7, 8.91, 8.71, 8.43, 8.73])
+        rs_1 = returns.returns(prices_1, drop_first=False)
+        tm.assert_series_equal(
+            rs_1, pd.Series([NaN, 0.0241379, -0.0224466, -0.032147, 0.035587])
+        )
+
+        prices_2 = pd.Series([10.66, 11.08, 10.71, 11.59, 12.11])
+        rs_2 = returns.returns(prices_2, drop_first=False)
+        tm.assert_series_equal(
+            rs_2, pd.Series([NaN, 0.0393996, -0.0333935, 0.0821661, 0.0448662])
+        )
+
+        # Coherence check
+        tm.assert_series_equal(rs_1, prices_1 / prices_1.shift(1) - 1)
+        tm.assert_series_equal(rs_2, prices_2 / prices_2.shift(1) - 1)
+
+    def test_series_returns_with_drop(self):
+        # Introduction to Portfolio Construction and Analysis with Python. EDHEC-Risk
+        prices_1 = pd.Series([8.7, 8.91, 8.71, 8.43, 8.73])
+        rs_1 = returns.returns(prices_1)
+        tm.assert_series_equal(
+            rs_1.reset_index(drop=True),
+            pd.Series([0.0241379, -0.0224466, -0.032147, 0.035587]),
+        )
+
+        prices_2 = pd.Series([10.66, 11.08, 10.71, 11.59, 12.11])
+        rs_2 = returns.returns(prices_2)
+        tm.assert_series_equal(
+            rs_2.reset_index(drop=True),
+            pd.Series([0.0393996, -0.0333935, 0.0821661, 0.0448662]),
+        )
+
+    def test_frame_returns(self):
+        # Introduction to Portfolio Construction and Analysis with Python. EDHEC-Risk
+        prices = pd.DataFrame(
+            {
+                "stock_1": [8.7, 8.91, 8.71, 8.43, 8.73],
+                "stock_2": [10.66, 11.08, 10.71, 11.59, 12.11],
+            }
+        )
+        rs = returns.returns(prices, drop_first=False)
+
+        expected = pd.DataFrame(
+            {
+                "stock_1": [NaN, 0.0241379, -0.0224466, -0.032147, 0.035587],
+                "stock_2": [NaN, 0.0393996, -0.0333935, 0.0821661, 0.0448662],
+            }
+        )
+
+        tm.assert_frame_equal(rs, expected)
+
+        # Coherence check
+        tm.assert_frame_equal(rs, prices / prices.shift(1) - 1)
