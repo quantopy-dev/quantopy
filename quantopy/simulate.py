41b715ecef2007b1185cb13effb8f62c425d68cc
Ismael Jimenez <ijimenezm@hotmail.es>
2021-05-23 21:19:22 +0200

    add stock price simulator

---
+++ quantopy/simulate.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def stock_price(mu: float, sigma: float, size: int, initial_price: float = 1.0):
+    simulated_returns = np.random.normal(mu, sigma, size) + 1
+    simulated_price = simulated_returns.cumprod() * initial_price
+    simulated_price = np.insert(simulated_price, 0, initial_price, axis=0)
+
+    return simulated_price
