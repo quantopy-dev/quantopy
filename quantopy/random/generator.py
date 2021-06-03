b46e85c64131f9feef5a4fbfb046ef7cd94587e6
Ismael <ismaelJimenez@users.noreply.github.com>
2021-06-03 19:14:20 +0200

    ENH_ add draft of prices simulator

--- quantopy/random/generator.py
+++ quantopy/random/generator.py
@@ -7,6 +7,40 @@ import quantopy as qp
 from quantopy._typing import PythonScalar
 
 
+def geometric_brownian_motion(
+    mu: np.ndarray, sigma: np.ndarray, size: int
+) -> np.ndarray:
+    """Generate random simple returns from a geometric brownian motion.
+
+    Parameters
+    ----------
+    mu : float or array_like of floats
+        Mean (“centre”) of the distribution.
+
+    sigma : float or array_like of floats
+       Standard deviation (spread or “width”) of the distribution. Must be non-negative.
+
+    size : int or tuple of ints, optional
+        Output shape. If the given shape is, e.g., (m, n, k), then m * n * k samples
+        are drawn. If size is None (default), a single value is returned if mu and sigma
+        are both scalars. Otherwise, np.broadcast(mu, sigma).size samples are drawn.
+
+    Returns
+    -------
+    out : ndarray or scalar
+        Drawn samples from a geometric brownian motion.
+
+    References
+    ----------
+    .. [1] "Geometric Brownian Motion", *Wikipedia*, https://en.wikipedia.org/wiki/Geometric_Brownian_motion.
+    """
+    dt = 1
+    brownian_increments = np.random.normal(0, np.sqrt(dt), (size,) + mu.shape)
+    simulated_returns = (mu - sigma ** 2 / 2) * dt + brownian_increments
+
+    return simulated_returns
+
+
 @overload
 def returns(
     mu: PythonScalar, sigma: PythonScalar, size: Optional[int] = None, method: str = ...
@@ -68,13 +102,35 @@ def returns(mu, sigma, size=None, method="basic"):
         return qp.ReturnDataFrame(simulated_returns)
 
 
-def geometric_brownian_motion(
-    mu: np.ndarray, sigma: np.ndarray, size: int
-) -> np.ndarray:
-    """Generate random simple returns from a geometric brownian motion.
+@overload
+def prices(
+    initial_price: float,
+    mu: PythonScalar,
+    sigma: PythonScalar,
+    size: Optional[int] = None,
+    method: str = ...,
+) -> qp.ReturnSeries:
+    ...
+
+
+@overload
+def prices(
+    initial_price: float,
+    mu: Union[List[int], List[float]],
+    sigma: Union[List[int], List[float]],
+    size: Optional[int] = None,
+    method: str = ...,
+) -> qp.ReturnDataFrame:
+    ...
+
+
+def prices(initial_price, mu, sigma, size=None, method="basic"):
+    """Generate price evolution from a given method.
 
     Parameters
     ----------
+    initial_price : float or array_like of floats
+
     mu : float or array_like of floats
         Mean (“centre”) of the distribution.
 
@@ -86,17 +142,25 @@ def geometric_brownian_motion(
         are drawn. If size is None (default), a single value is returned if mu and sigma
         are both scalars. Otherwise, np.broadcast(mu, sigma).size samples are drawn.
 
+    method: str
+        Method used for returns generation
+
     Returns
     -------
     out : ndarray or scalar
-        Drawn samples from a geometric brownian motion.
+        Generated prices.
 
     References
     ----------
-    .. [1] "Geometric Brownian Motion", *Wikipedia*, https://en.wikipedia.org/wiki/Geometric_Brownian_motion.
+    .. [1] "Normal distribution", *Wikipedia*, https://en.wikipedia.org/wiki/Normal_distribution.
     """
-    dt = 1
-    brownian_increments = np.random.normal(0, np.sqrt(dt), (size,) + mu.shape)
-    simulated_returns = (mu - sigma ** 2 / 2) * dt + brownian_increments
+    simulated_returns = returns(mu, sigma, size=None, method="basic")
 
-    return simulated_returns
+    if method == "gbm":
+        simulated_returns = np.exp(simulated_returns)
+    else:
+        raise ValueError(f"Invalid method {method}")
+
+    simulated_prices = initial_price * simulated_returns.cumprod()
+
+    return simulated_prices
