193a5adab2f1f276dd5e93c99bdea705a79eef4a
Ismael <ismaelJimenez@users.noreply.github.com>
2021-06-12 18:19:26 +0200

    DOC: Remove unused

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -238,14 +238,14 @@ html_theme_options = {
 # A shorter title for the navigation bar.  Default is the same as html_title.
 # html_short_title = None
 
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-html_logo = "../../web/pandas/static/img/quantopy.png"
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = "quantopy.png"
 
 html_css_files = [
     "css/getting_started.css",
@@ -255,7 +255,7 @@ html_css_files = [
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "../../web/pandas/static/img/favicon.ico"
+html_favicon = "favicon.ico"
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
